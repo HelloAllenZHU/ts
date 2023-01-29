534e1ff95c10194bb40b628e525398c33b2b3563
Allen ZHU <linux0824@gmail.com>
2023-01-29 11:21:20 +0800

    第一次提交

---
+++ hello/app.js
@@ -0,0 +1,5 @@
+function Hello(person) {
+    return 'Hello ' + person;
+}
+var user = 'Allen';
+console.log(Hello(user));
---
+++ hello/app.ts
@@ -0,0 +1,6 @@
+function Hello( person : string ) {
+    return 'Hello ' + person;
+}
+
+let user ='Allen';
+console.log( Hello( user ) );
